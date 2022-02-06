6d21e6e188da07532b596f23d6694cb6eecc74ad
RemLampa <rem@remlampa.com>
2022-02-06 23:54:46 +0800

    :sparkles: add solution

---
+++ src/components/PerspectiveBackground/PerspectiveBackground.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./PerspectiveBackground.module.css";
+
+const PerspectiveBackground = () => (
+  <div className={styles.perspectiveBackground} />
+);
+
+export default PerspectiveBackground;
